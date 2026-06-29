645ed76c05bc0a7c242c2f31c2f6c19f59f8b7fc
JJPbeste <salimhousni2010@gmail.com>
2026-06-29 19:24:54 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("============================\n " "Dein Persönliches Haustier\n============================")
+
+
+def main():
+    print ("Wie möchtest du dein Haustier nennen?\n>", end="")
+    name = input()
+    print(f"Hallo! Ich bin {name}!\nSchön dich kennenzulernen!")
+    print("Ich habe Hunger...")
+
+main()
